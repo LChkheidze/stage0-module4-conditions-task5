101c5feed04f2688f6c31f257fe287b752bacd98
LChkheidze <levanchx@gmail.com>
2023-05-10 01:24:02 +0400

    Switch statement, break, default

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        String aYear = (year % 4 != 0 || (year % 100 == 0 && year % 400 != 0))? "not leap" : "leap";
+
+        System.out.println(aYear);
     }
 }
